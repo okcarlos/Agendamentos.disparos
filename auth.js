955404e3b3db35e7053de14fda1871fea990e9e7
okcarlos <okcarlos122@gmail.com>
2026-07-19 13:13:22 -0300

    Refactor authorization to use environment variables

--- auth.js
+++ auth.js
@@ -1,87 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
-
-const SCOPES = [
-    "https://www.googleapis.com/auth/drive"
-];
-
-const TOKEN_PATH = path.join(__dirname, "token.json");
-const CREDENTIALS_PATH = path.join(__dirname, "client_secret.json");
-
-async function loadSavedCredentials() {
-
-    try {
-
-        const content = fs.readFileSync(
-            CREDENTIALS_PATH
-        );
-
-        const credentials = JSON.parse(content);
-
-
-        const {client_secret, client_id, redirect_uris} =
-        credentials.installed || credentials.web;
-
-
-        const auth = new google.auth.OAuth2(
-            client_id,
-            client_secret,
-            redirect_uris[0]
-        );
-
-
-        const token = JSON.parse(
-            fs.readFileSync(TOKEN_PATH)
-        );
-
-
-        auth.setCredentials(token);
-
-
-        return auth;
-
-
-    }
-
-    catch(error) {
-
-        console.log("Sem credenciais salvas");
-
-        return null;
-
-    }
-
-}
-
-async function saveCredentials(client) {
-
-    fs.writeFileSync(
-        TOKEN_PATH,
-        JSON.stringify(client.credentials)
-    );
-
-}
 
 async function authorize() {
 
-    let auth = await loadSavedCredentials();
+    const credentials = JSON.parse(
+        process.env.GOOGLE_CREDENTIALS
+    );
 
-    if (auth) return auth;
+    const token = JSON.parse(
+        process.env.GOOGLE_TOKEN
+    );
 
-    auth = await authenticate({
+    const { client_secret, client_id, redirect_uris } =
+        credentials.installed || credentials.web;
 
-        scopes: SCOPES,
+    const auth = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+    );
 
-        keyfilePath: CREDENTIALS_PATH
-
-    });
-
-    await saveCredentials(auth);
+    auth.setCredentials(token);
 
     return auth;
-
 }
 
 module.exports = authorize;

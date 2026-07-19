903147c8c670ee98e76644e9d33f488b943612cc
okcarlos <okcarlos122@gmail.com>
2026-07-19 12:53:54 -0300

    Refactor authorization to use saved credentials

--- auth.js
+++ auth.js
@@ -1,22 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
+
+const SCOPES = [
+    "https://www.googleapis.com/auth/drive"
+];
+
+const TOKEN_PATH = path.join(__dirname, "token.json");
+const CREDENTIALS_PATH = path.join(__dirname, "client_secret.json");
+
+async function loadSavedCredentials() {
+
+    try {
+
+        const content = fs.readFileSync(
+            CREDENTIALS_PATH
+        );
+
+        const credentials = JSON.parse(content);
+
+
+        const {client_secret, client_id, redirect_uris} =
+        credentials.installed || credentials.web;
+
+
+        const auth = new google.auth.OAuth2(
+            client_id,
+            client_secret,
+            redirect_uris[0]
+        );
+
+
+        const token = JSON.parse(
+            fs.readFileSync(TOKEN_PATH)
+        );
+
+
+        auth.setCredentials(token);
+
+
+        return auth;
+
+
+    }
+
+    catch(error) {
+
+        console.log("Sem credenciais salvas");
+
+        return null;
+
+    }
+
+}
+
+async function saveCredentials(client) {
+
+    fs.writeFileSync(
+        TOKEN_PATH,
+        JSON.stringify(client.credentials)
+    );
+
+}
 
 async function authorize() {
 
-    const credentials = JSON.parse(
-        process.env.GOOGLE_SERVICE_ACCOUNT
-    );
+    let auth = await loadSavedCredentials();
 
-    const auth = new google.auth.GoogleAuth({
+    if (auth) return auth;
 
-        credentials,
+    auth = await authenticate({
 
-        scopes: [
-            "https://www.googleapis.com/auth/drive"
-        ]
+        scopes: SCOPES,
+
+        keyfilePath: CREDENTIALS_PATH
 
     });
 
-    return await auth.getClient();
+    await saveCredentials(auth);
+
+    return auth;
 
 }
 

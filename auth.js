3ab89a13f8e5aa8e88ae41920a08b797637ca4e3
okcarlos <okcarlos122@gmail.com>
2026-07-18 22:32:31 -0300

    Update authorization method to use GoogleAuth
    Refactor authorization to use Google Service Account credentials from environment variable.

--- auth.js
+++ auth.js
@@ -1,86 +1,22 @@
-const fs = require("fs");
-const path = require("path");
-const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
-
-const SCOPES = [
-    "https://www.googleapis.com/auth/drive"
-];
-
-const TOKEN_PATH = path.join(__dirname, "token.json");
-const CREDENTIALS_PATH = path.join(__dirname, "client_secret.json");
-
-async function loadSavedCredentials() {
-
-    try {
-
-        const content = fs.readFileSync(
-            CREDENTIALS_PATH
-        );
-
-        const credentials = JSON.parse(content);
-
-
-        const {client_secret, client_id, redirect_uris} =
-        credentials.installed || credentials.web;
-
-
-        const auth = new google.auth.OAuth2(
-            client_id,
-            client_secret,
-            redirect_uris[0]
-        );
-
-
-        const token = JSON.parse(
-            fs.readFileSync(TOKEN_PATH)
-        );
-
-
-        auth.setCredentials(token);
-
-
-        return auth;
-
-
-    }
-
-    catch(error) {
-
-        console.log("Sem credenciais salvas");
-
-        return null;
-
-    }
-
-}
-
-async function saveCredentials(client) {
-
-    fs.writeFileSync(
-        TOKEN_PATH,
-        JSON.stringify(client.credentials)
-    );
-
-}
 
 async function authorize() {
 
-    let auth = await loadSavedCredentials();
+    const credentials = JSON.parse(
+        process.env.GOOGLE_SERVICE_ACCOUNT
+    );
 
-    if (auth) return auth;
+    const auth = new google.auth.GoogleAuth({
 
-    auth = await authenticate({
+        credentials,
 
-        scopes: SCOPES,
-
-        keyfilePath: CREDENTIALS_PATH
+        scopes: [
+            "https://www.googleapis.com/auth/drive"
+        ]
 
     });
 
-    await saveCredentials(auth);
-
-    return auth;
+    return await auth.getClient();
 
 }
 
